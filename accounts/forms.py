9f1c3498d3ba01d578815a83587d2937a4aa1861
Abdul Motin <infomotin@users.noreply.github.com>
2024-02-27 13:19:20 +0600

    first models error

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,3 +9,20 @@ class CustomUserCreationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email', 'username', 'first_name', 'last_name','phone_number','password']
+    def clean(self):
+        cleaned_data = super(CustomUserCreationForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm password does not match"
+            )
+
+    # def save(self, commit=True):
+    #     user = super(CustomUserCreationForm, self).save(commit=False)
+    #     user.set_password(self.cleaned_data["password"])
+    #     if commit:
+    #         user.save()
+    #     return user
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,45 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from . forms import CustomUserCreationForm
+from . models import User
 
 # Create your views here.
 def registerUser(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if(form.is_valid()):
-            form.save()
+        
+            # Create User using form 
+            # password = form.cleaned_data.get('password')
+            # form.save(commit=False)
+            # form.role = User.CUSTOMER
+            # form.set_password(password)
+            # form.is_active = True
+            # print(form)
+            # form.save()
+
+            # Create the User using create_user method
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+
+            user = User.objects.createuser(
+                first_name = first_name,
+                last_name = last_name,
+                username = username,
+                email = email,
+                password = password
+            )
+            user.role = User.CUSTOMER
+            user.is_active = True
+            user.phone_number = form.cleaned_data.get('phone_number')
+            user.save()
+
             return redirect('registerUser')
+        else:
+            print(form.errors)
     else:
         form = CustomUserCreationForm()
     context = {'form': form}
