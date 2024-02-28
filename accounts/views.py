d40d1bb758c62ecd1a99fe9d158fb2128cd3069e
Abdul Motin <infomotin@users.noreply.github.com>
2024-02-28 14:53:27 +0600

    vendor admin site add

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from . forms import CustomUserCreationForm
-from . models import User
+from . models import User,UserProfile
+from vendor.forms import VendorCreationForm
 # message
 from django.contrib import messages
 
@@ -49,5 +50,40 @@ def registerUser(request):
 
 
 def registerVendor(request):
-    
-    return render(request, 'accounts/registerVendor.html')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        v_form = VendorCreationForm(request.POST,request.FILES)
+        print(form.is_valid(), v_form.is_valid())
+        if form.is_valid() and v_form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+
+            user = User.objects.createuser(
+                first_name = first_name,
+                last_name = last_name,
+                username = username,
+                email = email,
+                password = password
+            )
+            user.role = User.RESTARUNT
+            user.is_active = False
+            user.phone_number = form.cleaned_data.get('phone_number')
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('registerVendor')
+        else:
+            print(form.errors, v_form.errors)
+    else:
+
+        form = CustomUserCreationForm()
+        v_form = VendorCreationForm()
+    context = {'form': form, 'v_form': v_form}
+    return render(request, 'accounts/registerVendor.html',context)
--- vendor/admin.py
+++ vendor/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from vendor.models import vendor
 # Register your models here.
-admin.site.register(vendor)
+class vendorAdmin(admin.ModelAdmin):
+    list_display = ('user','vendor_name', 'is_approved', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+    list_editable = ('is_approved',)
+    search_fields = ('vendor_name',)
+    list_filter = ('is_approved', 'created_at')
+
+
+
+admin.site.register(vendor, vendorAdmin)
